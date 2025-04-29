72fa629c62f4315741f3af3896751d646d11bd21
AmpF5 <lolc44t@gmail.com>
2025-04-29 18:21:53 +0200

    Add list state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use color_eyre::Result;
+use ratatui::{buffer::Buffer, layout::{Alignment, Rect}, style::{Color, Style}, widgets::{block::Position, ListItem, ListState, Widget}};
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
-    DefaultTerminal, Frame,
-    style::Stylize,
-    text::Line,
-    widgets::{Block, Paragraph},
+    layout::{Constraint, Layout}, style::{palette::tailwind::SLATE, Modifier, Stylize}, text::Line, widgets::{Block, List, ListDirection, Paragraph}, DefaultTerminal, Frame
 };
+static SELECTED_STYLE: Style = Style::new().bg(SLATE.c900).add_modifier(Modifier::BOLD);
+const NORMAL_ROW_BG: Color = SLATE.c950;
+const ALT_ROW_BG_COLOR: Color = SLATE.c900;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
@@ -14,12 +15,29 @@ fn main() -> color_eyre::Result<()> {
     ratatui::restore();
     result
 }
+#[derive(Debug, Default)]
+pub struct File {
+    path: String,
+    is_selected: bool
+}
 
-/// The main application which holds the state and logic of the application.
+impl File {
+    fn init(path: String) -> File {
+        File {path, is_selected: false} 
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct FileList {
+    items: Vec<File>,
+    state: ListState
+}
+
 #[derive(Debug, Default)]
 pub struct App {
-    /// Is the application running?
     running: bool,
+    files_from: FileList,
+    files_to: FileList
 }
 
 impl App {
@@ -31,6 +49,13 @@ impl App {
     /// Run the application's main loop.
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         self.running = true;
+        self.files_from = FileList {
+            items: vec![
+                File::init(String::from("From 1")),
+                File::init(String::from("From 2")),
+            ],
+            state: ListState::default(),
+        };
         while self.running {
             terminal.draw(|frame| self.render(frame))?;
             self.handle_crossterm_events()?;
@@ -38,32 +63,82 @@ impl App {
         Ok(())
     }
 
-    /// Renders the user interface.
-    ///
-    /// This is where you add new widgets. See the following resources for more information:
-    ///
-    /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
-    /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
     fn render(&mut self, frame: &mut Frame) {
-        let title = Line::from("Ratatui Simple Template")
-            .bold()
-            .blue()
-            .centered();
-        let text = "Hello, Ratatui!\n\n\
-            Created using https://github.com/ratatui/templates\n\
-            Press `Esc`, `Ctrl-C` or `q` to stop running.";
-        frame.render_widget(
-            Paragraph::new(text)
-                .block(Block::bordered().title(title))
-                .centered(),
-            frame.area(),
-        )
+        use Constraint::{Fill, Length, Min};
+
+        let vertical = Layout::vertical([Length(1), Min(0), Length(3)]);
+        let [title_area, main_area, status_area] = vertical.areas(frame.area());
+
+        let horizontal = Layout::horizontal([Fill(1); 2]);
+        let [left_area, right_area] = horizontal.areas(main_area);
+
+        // let mut state = ListState::default();
+
+        frame.render_widget(Block::bordered().title("Blazingly fast moving files").title_alignment(Alignment::Center), title_area);
+
+
+        frame.render_widget(Block::bordered().title("Import to"), right_area);
+
+        self.render_from_list(left_area, frame);
+
+        self.render_footer(status_area, frame);
     }
 
-    /// Reads the crossterm events and updates the state of [`App`].
-    ///
-    /// If your application needs to perform work in between handling events, you can use the
-    /// [`event::poll`] function to check if there are any events available with a timeout.
+    fn render_footer(&mut self, area: Rect, frame: &mut Frame) {
+        frame.render_widget(Paragraph::new("Use ↓↑ to move, ← to unselect, → to change status, g/G to go top/bottom.")
+            .centered()
+            .block(Block::bordered().title("Options")), 
+            area);
+    }
+
+    fn render_from_list(&mut self, area: Rect, frame: &mut Frame) {
+        let items: Vec<ListItem> = self
+            .files_from
+            .items
+            .iter()
+            .enumerate()
+            .map(|(i, file)| {
+                let color = alternate_colors(i);
+                ListItem::from(file.path.clone()).bg(color)
+            })
+            .collect();
+
+        let files_from_list = List::new(items)
+            .block(Block::bordered().title("Import from"))
+            .style(Style::new().white())
+            .highlight_style(Style::new().italic())
+            .highlight_symbol(">>")
+            .repeat_highlight_symbol(true);
+
+        frame.render_stateful_widget(files_from_list, area, &mut self.files_from.state);
+
+    }
+
+    /// Handles the key events and updates the state of [`App`].
+    fn on_key_event(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Char('q')  => self.quit(),
+            KeyCode::Down => self.select_next(),
+            KeyCode::Up => self.select_previous(),
+            KeyCode::Char(' ') => self.change_status(),
+            _ => {}
+        }
+    }
+
+    fn select_next(&mut self) {
+        self.files_from.state.select_next();
+    }
+
+    fn select_previous(&mut self) {
+        self.files_from.state.select_previous();
+    }
+
+    fn change_status(&mut self) {
+        if let Some(i) = self.files_from.state.selected() {
+            self.files_from.items[i].is_selected = true
+        }
+    }
+
     fn handle_crossterm_events(&mut self) -> Result<()> {
         match event::read()? {
             // it's important to check KeyEventKind::Press to avoid handling key release events
@@ -75,18 +150,16 @@ impl App {
         Ok(())
     }
 
-    /// Handles the key events and updates the state of [`App`].
-    fn on_key_event(&mut self, key: KeyEvent) {
-        match (key.modifiers, key.code) {
-            (_, KeyCode::Esc | KeyCode::Char('q'))
-            | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
-            // Add other key handlers here.
-            _ => {}
-        }
-    }
-
-    /// Set running to false to quit the application.
     fn quit(&mut self) {
         self.running = false;
+    }
+    
+}
+
+const fn alternate_colors(i: usize) -> Color {
+    if i % 2 == 0 {
+        NORMAL_ROW_BG
+    } else {
+        ALT_ROW_BG_COLOR
     }
 }
